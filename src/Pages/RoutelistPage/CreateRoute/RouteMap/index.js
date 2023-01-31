f6f3328ff36310660f43eb36be074e27808516b0
Burhan Ahmed <mburhanahmed@hotmail.com>
2023-01-31 18:55:11 +0500

    markers added

--- src/Pages/RoutelistPage/CreateRoute/RouteMap/index.js
+++ src/Pages/RoutelistPage/CreateRoute/RouteMap/index.js
@@ -1,51 +1,75 @@
-import React, {useEffect, useRef} from "react";
-import GoogleMapReact from "google-map-react";
-import Marker from "./Marker/marker";
+import React from "react";
+import { Fragment, useEffect, useState, useRef } from "react";
+import { useGoogleMaps } from "react-hook-google-maps";
 
+export default function RouteMap(props) {
+  const prevMarkersRef = useRef([]);
 
+  // incoming location to set
+  let point = {
+    lat: 35,
+    lng: -100,
+  };
 
-export default function RouteMap(props){
+  let dest = {
+    lat: 34.99,
+    lng: -100.1,
+  };
 
+  // Map options
+  const { ref, map, google } = useGoogleMaps(
+    "AIzaSyAoyevYqWkjKEJjq6vPXzfhulxkIecZhX0",
+    {
+      zoom: 6,
+      center: point,
+    }
+  );
+  useEffect(() => {
+    if (map) {
+      // ADD MARKER
+      const m = addMarker();
+      clearMarkers(prevMarkersRef.current); //clear prev markers
+      prevMarkersRef.current.push(m);
+      map.setCenter(point);
+      //Add Directions
+      let directionsService = new google.maps.DirectionsService();
+      let directionsRenderer = new google.maps.DirectionsRenderer();
+      directionsRenderer.setMap(map);
+      calcRoute(directionsService, directionsRenderer);
+    }
+  }, [point]);
 
-    const defaultProps = {
-        center:{
-            lat: 30.4390,
-            lng: 72.3552
-        },
-        zoom: 11
+  // SIDE FUNCTIONS
+  function addMarker() {
+    return new window.google.maps.Marker({
+      position: point,
+      map: map,
+    });
+  }
+  function clearMarkers(markers) {
+    for (let m of markers) {
+      m.setMap(null);
+    }
+  }
+
+  function calcRoute(directionsService, directionsRenderer) {
+    let request = {
+      origin: point,
+      destination: dest,
+      travelMode: "DRIVING",
     };
+    directionsService.route(request, function (result, status) {
+      if (status == "OK") {
+        directionsRenderer.setDirections(result);
+      }
+    });
+  }
 
-
-    const renderMarkers = (map, maps) => {
-
-    };
-
-
-
-    return(
-        <>
-            <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyAoyevYqWkjKEJjq6vPXzfhulxkIecZhX0" }}
-                    defaultCenter={defaultProps.center}
-                    defaultZoom={defaultProps.zoom}
-                    yesIWantToUseGoogleMapApiInternals={true}
-                    onGoogleApiLoaded={({map, maps}) => renderMarkers(map, maps)}
-                >
-
-
-                    <Marker
-                        draggable={false}
-                        position={defaultProps.center}
-                        map
-                        // lat={props.lat}
-                        // lng={props.lng}
-                        name="My Marker"
-                    />
-
-                </GoogleMapReact>
-            </div>
-        </>
-    )
-
+  return (
+    <>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <div ref={ref} style={{ height: "100vh", width: "100%" }} />
+      </div>
+    </>
+  );
 }
