b7fb5b7edb10ea494caa2e4f02b6e42e082644bf
Waqar Yazdani <waqaryazdani2@gmail.com>
2023-06-01 17:19:54 +0500

    CHanges
    Former-commit-id: 098c66df3abf68a10c086beb5fcf991cbe0d3147

--- src/Pages/RoutelistPage/EditRouteList/EditRoute.js
+++ src/Pages/RoutelistPage/EditRouteList/EditRoute.js
@@ -171,13 +171,31 @@ const EditRoute = (props) => {
   useEffect(() => {
     console.log(`historicalData: ${JSON.stringify(historicalData)}`);
   }, [historicalData]);
+  function isValidCoordinate(value) {
+    return /^-?([1-8]?[0-9]\.{1}\d{1,6}|90\.{1}0{1,6})$/.test(value);
+  }
   const submitForm = async (event) => {
     event.preventDefault();
     const form = event.currentTarget;
+    const errors = coordinatesData.map((coordinate) => {
+      const latValid = isValidCoordinate(coordinate.lat);
+      const lngValid = isValidCoordinate(coordinate.lng);
+
+      if (latValid && lngValid) {
+        return '';
+      } else {
+        return 'error';
+      }
+
+    });
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
       setValidated(true);
+    }else if(errors.includes('error')) {
+      swal("Error", 'Invalid Route Entered', "error");
+    }else if(coordinates.length===0) {
+      swal("Error", 'Pls generate route', "error");
     }
     else {
       const payloadHistorical = [];
@@ -469,10 +487,21 @@ const EditRoute = (props) => {
 
   const addRouteToMap = () => {
     const dataCoordinates = [];
-    coordinatesData.map((data, index) => (
-        dataCoordinates.push([parseFloat(data.lng),parseFloat(data.lat)])
-    ))
-    setCoordinates(dataCoordinates);
+    const isDataValid = coordinatesData.every(obj => {
+      return Object.values(obj).every(value => {
+        if (isValidCoordinate(value)) {
+
+          return value
+        }
+        //return value !== null && value !== '';
+      });
+    });
+    if (isDataValid) {
+      coordinatesData.map((data, index) => (
+          dataCoordinates.push([parseFloat(data.lng),parseFloat(data.lat)])
+      ))
+      setCoordinates(dataCoordinates);
+    }
   }
   const addHistoricalToMap = () => {
     const dataCoordinates = [];
